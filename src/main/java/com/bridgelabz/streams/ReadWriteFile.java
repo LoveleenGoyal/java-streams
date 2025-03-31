9a36e58e6a1c9bcad386347855486420c3e9897b
LoveleenGoyal <loveleengoyal26@gmail.com>
2025-03-31 09:48:18 +0530

    [Loveleen] ADD: java streams

--- src/main/java/com/bridgelabz/streams/ReadWriteFile.java
+++ src/main/java/com/bridgelabz/streams/ReadWriteFile.java
@@ -1,4 +1,20 @@
 package com.bridgelabz.streams;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class ReadWriteFile {
+    public static void main(String[] args) {
+        try (FileInputStream fis = new FileInputStream("input.txt");
+             FileOutputStream fos = new FileOutputStream("output.txt")) {
+            int byteData;
+            while ((byteData = fis.read()) != -1) {
+                fos.write(byteData);
+            }
+            System.out.println("File copied successfully.");
+        } catch (IOException e) {
+            System.out.println("File does not exist.");
+        }
+    }
 }
